0308540be694aa9022b1104ddc40aee9464ca13b
Spencer Lindsay <spence@lindsaydigital.com>
2021-01-13 11:31:57 -0800

    Added PlayCanvas TestScene

---
+++ src/Vortex/__settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1067806.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 40881421, 40881422, 40881437, 40881432, 40881420, 40881435, 40937881, 40881433, 41025440, 41025441, 41025442 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/40937646/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/40937647/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/40937645/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/41025159/1/basis.wasm.js', 'wasmUrl' : 'files/assets/41025160/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/41025158/1/basis.js', 'preload' : false},
+];
