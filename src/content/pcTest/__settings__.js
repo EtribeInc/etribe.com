0de1d008d2144615617c87550fb92b0ff52a9c99
Paul <paul.wolff.dd@gmail.com>
2022-09-06 14:21:28 +0200

    - added pc 3d / html hybrid test

---
+++ src/content/pcTest/__settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1512037.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 99332457, 99332462, 99332468, 99332460, 99333018, 99333019, 99333020, 100687708, 100687709, 100688962, 100690259, 100690287, 100692671 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/100688955/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/100688956/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/100688954/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/99332980/1/basis.wasm.js', 'wasmUrl' : 'files/assets/99332978/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/99332979/1/basis.js', 'preload' : false},
+];
