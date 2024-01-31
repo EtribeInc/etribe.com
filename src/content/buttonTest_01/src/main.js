15ea1ebe8aaa82a25e331abf9a43491eedafea75
Spencer Lindsay <spence@lindsaydigital.com>
2024-01-31 10:41:19 -0800

    local assets now.

--- src/content/buttonTest_01/src/main.js
+++ src/content/buttonTest_01/src/main.js
@@ -2,4 +2,4 @@ import { Application } from '@splinetool/runtime';
 
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
-app.load('https://prod.spline.design/R4IsZQPZ0lRlVQi9/scene.splinecode');
+app.load('./scene.splinecode');
