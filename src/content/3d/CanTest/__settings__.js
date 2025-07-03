ffebfeb52ddbd2b6098b82e8df30b50e84b3eb21
Spencer Lindsay <spence@lindsaydigital.com>
2025-07-03 15:29:45 -0700

    MadPopCanTest

---
+++ src/content/3d/CanTest/__settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2274995.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 239336851, 239336859, 239336860, 239336868, 239336999, 239337000, 239337001 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
